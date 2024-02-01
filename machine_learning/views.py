6befbc4782f1e8c7a81d90f7c74badfd616920e2
Manish@123 <kushwahamanish818@gmail.com>
2024-02-01 10:42:25 +0530

    new commit

--- machine_learning/views.py
+++ machine_learning/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,HttpResponse
+from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
 import requests
 
@@ -6,19 +6,16 @@ import requests
 
 def scan_face(request):
     if request.method == "POST":
-        image = request.FILES['video']  # get the uploaded file
-        url = "http://115.241.73.227/upload/"
+        video = request.FILES['video']  # get the uploaded file
+        url = "http://115.241.73.227/video/analysis/"
 
         payload = {'user': '8299037804'}
-        files=[
-        ('video_file',(image,open(image,'rb'),'application/octet-stream'))
-        ]
+        files = {'video_file': video} 
         headers = {}
 
         response = requests.request("POST", url, headers=headers, data=payload, files=files)
 
         print(response.text)
-        print(image)
         return HttpResponse(response.text)
     return render(request,"upload.html")
 
@@ -26,17 +23,17 @@ def analized_video_detail(request):
     return render(request,"video_detail.html")
 
 def analized_video_list(request):
-    user = 8299037804
-    url = f"http://115.241.73.227/analysed/video/list/?user={user}"
+    user = "8299037804"
+    url = f"http://115.241.73.227/video/analysed/video/list/?user={8299037804}"
 
     payload = {}
     headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
-
-    print(response.text)
+    response_data = response.json()
+    print(response_data)
     return render(request,"analize_video_list.html",{
-        "all_data":response.text
+        "all_data":response_data["data"],
     })
 
 
--- mont_project/settings.py
+++ mont_project/settings.py
@@ -121,8 +121,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 X_FRAME_OPTIONS = 'ALLOWALL'
 
